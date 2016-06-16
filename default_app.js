c90a639de133078b5c8d7fd1bd492f302cf165b5
116356754 <116356754@qq.com>
2016-06-16 09:27:06 +0800

    first commit

---
+++ default_app.js
@@ -0,0 +1,46 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var path = require('path');
+var conf = require('./config');
+var fs = require('fs');
+
+var mainWindow = null;
+app.commandLine.appendSwitch('ppapi-flash-path', __dirname + '/plugins/pepflashplayer.dll');
+
+//var child =null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    //child.kill('SIGTERM');
+    app.quit();
+});
+
+const ppflashsolpath ='/Pepper Data/Shockwave Flash/WritableRoot/#SharedObjects/KMED3JYF/macromedia.com/support/flashplayer/sys';
+
+
+app.on('ready', function () {
+
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: !conf.ISPRODUCT,
+        kiosk: conf.ISPRODUCT,
+        fullscreen:conf.ISPRODUCT,
+        alwaysOnTop:true,
+            'auto-hide-menu-bar': true,
+        'use-content-size': true,
+        'web-preferences': {
+            'plugins': true,
+            'web-security': false
+        }
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    //mainWindow.loadURL('http://www.macromedia.com/support/documentation/en/flashplayer/help/settings_manager04.html');//flash测试
+    mainWindow.focus();
+    if(!conf.ISPRODUCT)
+        mainWindow.openDevTools();
+    else
+        mainWindow.maximize();
+
+});
---
+++ index.js
@@ -0,0 +1,191 @@
+/**
+ * Created by Administrator on 2016/6/15.
+ */
+var app = require('electron').remote.app;
+
+var event = require('events');
+var EventEmitter = require('events').EventEmitter;
+var emitter = new EventEmitter();
+
+var fs = require('fs');
+var path =require('path');
+
+var StateMachine = require("./lib/state-machine.min.js");
+var config = require('./config');
+
+var timer1=null;//待机定时器
+
+var flashvars = {};
+var params = {
+    wmode: "transparent",
+    allowScriptAccess: "always",
+    scale: "exactFit",
+    play:true,
+    loop:false,
+    allowfullscreen : true,
+    swliveconnect:true
+};
+
+var attributes = { width:"100%", height:"100%" };
+
+function loadSWF(url, targetID){
+    //Check for existing SWF
+    if(isObject(targetID)){
+        //replace object/element with a new div
+        replaceSwfWithEmptyDiv(targetID);
+    }
+
+    //Embed SWF
+    if (swfobject.hasFlashPlayerVersion("9")) {
+        attributes.data=url;
+        var obj = swfobject.createSWF(attributes, params, targetID);
+    }
+}
+
+window.onload = function () {
+    loadSWF(path.join(__dirname,config.swfDir,config.waitSwf),'flashcontent');
+
+    setInterval(isPlayOver,500);
+};
+
+var fsm = StateMachine.create({
+    initial: '待机界面',
+    error: function(eventName, from, to, args, errorCode, errorMessage) {
+        console.error( 'event ' + eventName + ' was naughty :- ' + errorMessage);
+    },
+    events: [
+        { name: '一级界面指令',  from: '待机界面',  to: '一级界面' },
+        { name: '超时指令',  from: '一级界面',  to: '待机界面' },
+
+        { name: '展区简介指令', from: '一级界面', to: '展区简介'  },
+        { name: '宣传视频指令', from: '一级界面', to: '宣传视频'  },
+        { name: '荣誉墙指令', from: '一级界面', to: '荣誉墙'  },
+
+        { name: '返回指令',  from: '展区简介',  to: '一级界面' },
+        { name: '超时指令',  from: '展区简介',  to: '待机界面' },
+
+        { name: '返回指令',  from: '宣传视频',  to: '一级界面' },
+        { name: '播放完成指令',  from: '宣传视频',  to: '一级界面' },
+
+        { name: '返回指令',  from: '荣誉墙',  to: '一级界面' },
+        { name: '超时指令',  from: '荣誉墙',  to: '待机界面' },
+
+        ////////////////////////////////////////////////////////////////
+        { name: '领导关怀指令', from: '荣誉墙', to: '领导关怀'  },
+        { name: '主题活动指令', from: '荣誉墙', to: '主题活动'  },
+
+        { name: '超时指令',  from: '主题活动指令',  to: '荣誉墙' },
+        { name: '超时指令',  from: '主题活动指令',  to: '荣誉墙' },
+
+        ////////////////////////////////////////////////////////////////
+        { name: '儿童科技乐园指令', from: '展区简介', to: '儿童科技乐园'  },
+        { name: '儿童展区指令', from: '展区简介', to: '儿童展区'  },
+        { name: '基础科学展区指令', from: '展区简介', to: '基础科学展区'  },
+        { name: '电磁探秘指令', from: '展区简介', to: '电磁探秘'  },
+        { name: '科学表演指令', from: '展区简介', to: '科学表演'  },
+        { name: '声光体验指令', from: '展区简介', to: '声光体验'  },
+        { name: '数学魅力指令', from: '展区简介', to: '数学魅力'  },
+        { name: '数字生活指令', from: '展区简介', to: '数字生活'  },
+        { name: '运动旋律指令', from: '展区简介', to: '运动旋律'  },
+
+        { name: '播放完成指令',  from: '儿童科技乐园',  to: '展区简介' },
+        { name: '播放完成指令',  from: '儿童展区',  to: '展区简介' },
+        { name: '播放完成指令',  from: '基础科学展区',  to: '展区简介' },
+        { name: '播放完成指令',  from: '电磁探秘',  to: '展区简介' },
+        { name: '播放完成指令',  from: '科学表演',  to: '展区简介' },
+        { name: '播放完成指令',  from: '声光体验',  to: '展区简介' },
+        { name: '播放完成指令',  from: '数学魅力',  to: '展区简介' },
+        { name: '播放完成指令',  from: '数字生活',  to: '展区简介' },
+        { name: '播放完成指令',  from: '运动旋律',  to: '展区简介' }
+    ]});
+
+/////////////////////////////////////////////////////
+//mywork
+function readserialportport() {
+    var SerialPort = require("serialport").SerialPort
+    var serialPort = new SerialPort(config.COMMPORT, {
+        baudrate: config.COMMbaudrate,
+        autoOpen: false
+    });
+    serialPort.open(function(err){
+        if(err) {
+            emitter.emit('err', '串口打开失败:' + err.message);
+            return console.log('Error opening port: ', err.message);
+        }
+    });
+
+    serialPort.on("open", function (err) {
+        console.log('open');
+        serialPort.on('data', function (data) {
+            //alert(JSON.parse(data));
+            var buff = new Buffer(data, 'utf8'); //no sure about this
+            emitter.emit('cmd', buff.toString('hex'));
+        });
+    });
+}
+
+emitter.on('err',(e)=>{
+    alert(e);
+    return app.quit();
+});
+
+function isPlayOver()
+{
+    var flashMovie = swfobject.getObjectById('flashcontent');
+    var totalFrames = flashMovie.TotalFrames();
+    //// 4 is the index of the property for _currentFrame
+    var currentFrame = flashMovie.CurrentFrame();
+    //console.log(currentFrame+':'+totalFrames);
+    if(currentFrame +1 ==totalFrames)
+    {
+        if(fsm.can('播放完成指令'))
+        {
+            console.log('play over');
+            fsm.播放完成指令();
+        }
+
+        if(fsm.can('超时指令'))
+        {
+            console.log('开启无操作定时器');
+            if(time1 ==null)
+                timer1 = setTimeout(()=>fsm.超时指令(),config.timeout);
+        }
+    }
+}
+
+function playSwf(file)
+{
+    console.log('播放文件：'+file);
+    loadSWF(file,'flashcontent');
+    console.log(swfobject.getObjectById('flashcontent').TotalFrames());
+}
+
+fsm.onenterstate = function(event, from, to) {
+    console.log('进入'+to);
+    if(fs.existsSync(path.join(__dirname,config.swfDir,to+'.swf'))) {
+        console.log(path.join(__dirname,config.swfDir, to + '.swf') + '文件存在');
+        playSwf(path.join(__dirname,config.swfDir, to + '.swf'));
+    }
+    else
+        console.error('该状态的文件不存在');
+};
+
+fsm.onenter待机界面=function(event, from, to) {
+    console.log('进入待机界面');
+};
+
+//一旦有串口命令过来，立刻取消掉定时器，重新设置定时器，
+//////////////////////////////////////////////////////////////
+var cmdHandle = function(cmd) {
+    console.log('data received: ' + cmd);
+    cmd = 'C' + cmd;
+
+    if (timer1)
+    {
+        clearTimeout(timer1);
+        time1 = null;
+    }
+    fsm[config[cmd]]();
+};
+emitter.on('cmd',cmdHandle);
+readserialportport();
